48fcf506dc5f5d5f45b911eba3ee9c45ed00fe4b
jackvines <jack.vines@live.co.uk>
2023-03-31 21:32:52 +0100

    Minor updates

--- script2.js
+++ script2.js
@@ -20,27 +20,3 @@ button.addEventListener('click', (e) => {
     // Show thank you message element
     document.getElementById('thank_you').style.display = 'block';
 })
-
-window.addEventListener("load", function() {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-    });
-  });
-
-  var nextStep = document.querySelector('#close');
-              
-  nextStep.addEventListener('click', function (e) {
-    e.preventDefault();
-    // Hide first view
-    document.getElementById('my_form').style.display = 'block';
-
-    // Show thank you message element
-    document.getElementById('thank_you').style.display = 'none';
-  });
