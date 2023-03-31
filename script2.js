e13784aab943ae0d1ade18c22d6d853a00ac986e
Jack-Vines <jack.vines@live.co.uk>
2023-03-31 20:00:00 +0100

    Clean up

--- script2.js
+++ script2.js
@@ -3,11 +3,17 @@ const canvas = document.querySelector('#confetti');
 
 const jsConfetti = new JSConfetti();
 
+
+
 button.addEventListener('click', (e) => {
     jsConfetti.addConfetti({
-        emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
-    }).then(() => jsConfetti.addConfetti()
-    )
+        emojis: ['🍾', '🏄‍♂️', '🏄‍♀️', '🌊', '🏖', '🍹'],
+    }).then(() => jsConfetti.addConfetti({
+        confettiColors: [
+          '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+        ],
+      })
+    ).then(() => jsConfetti.addConfetti())
     // e.preventDefault();
      // Hide first view
     document.getElementById('rsvp-form').style.display = 'none';
